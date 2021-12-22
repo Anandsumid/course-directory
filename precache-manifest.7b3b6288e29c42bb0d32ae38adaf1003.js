f1267bc2e6a92a0d7743ecbc714ad4902bb9c523
lab_1_1x <lab_1_15@lab-1-15s-iMac.local>
2021-12-22 10:54:01 +0800

    Updates

---
+++ precache-manifest.7b3b6288e29c42bb0d32ae38adaf1003.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6b8c4512a86ed8ab412",
+    "url": "/Anandsumid/course-directory.git/static/css/main.37697fec.chunk.css"
+  },
+  {
+    "revision": "e6b8c4512a86ed8ab412",
+    "url": "/Anandsumid/course-directory.git/static/js/main.e6b8c451.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/Anandsumid/course-directory.git/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "a2fdc595cbd24a595547",
+    "url": "/Anandsumid/course-directory.git/static/js/runtime~main.a2fdc595.js"
+  },
+  {
+    "revision": "8c6267c6fa4b2f5d6d157830bbbfd6b6",
+    "url": "/Anandsumid/course-directory.git/index.html"
+  }
+];
